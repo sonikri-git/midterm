4c2544e6d45c45a81e56909d13089baee32148e6
Kritish Soni <kritishsoni786@gmail.com>
2025-10-14 20:23:30 -0400

    Added discount feature by Kritish Soni

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -1,5 +1,19 @@
 public class ProductManager {
     public static void main(String[] args) {
         System.out.println("ProductManager started.");
+
+        double price = 100.0;
+        double discountPercent = 15.0;
+        double discounted = calculateDiscount(price, discountPercent);
+        System.out.println("Discounted price: " + discounted);
+    }
+
+    // (Doc comment will be added in the next commit)
+    public static double calculateDiscount(double price, double percent) {
+        if (percent < 0 || percent > 100) {
+            throw new IllegalArgumentException("Percent must be between 0 and 100.");
+        }
+        double discount = price * (percent / 100.0);
+        return price - discount;
     }
 }
